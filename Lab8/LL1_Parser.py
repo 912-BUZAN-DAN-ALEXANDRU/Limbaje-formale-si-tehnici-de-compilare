9114a5d2ed68d0e559c6f29b3753a89d82c8ff93
Buzan Dan <42764054+912-BUZAN-DAN-ALEXANDRU@users.noreply.github.com>
2023-01-11 08:18:55 +0200

    Add files via upload

--- Lab8/LL1_Parser.py
+++ Lab8/LL1_Parser.py
@@ -1,12 +1,123 @@
+import sys
+import traceback
+
 from Grammar import Grammar
 import copy
 
+
 class LL_Parser(object):
-    def __init__(self, grammar:Grammar):
+    def __init__(self, grammar: Grammar):
         self.__grammar = grammar
         self.__firstTable = {}
         self.__followTable = {}
         self.EPSILON = "eps"
+        self.__parseTable = {}
+        self.__productionsRhs = []
+        self.generateParseTable()
+
+    @property
+    def grammar(self):
+        return self.__grammar
+
+    def generateParseTable(self):
+        rows = list()
+        rows.extend(self.__grammar.NonTerminals)
+        rows.extend(self.__grammar.Alphabet)
+        rows.append("$")
+
+        columns = list()
+        columns.extend(self.grammar.Alphabet)
+        columns.append("$")
+
+        for row in rows:
+            for col in columns:
+                self.__parseTable.update((row, col), ("err", -1))
+
+        for col in columns:
+            self.__parseTable.update((col, col), ("pop", -1))
+
+        self.__parseTable.update(("$", "$"), ("acc", -1))
+
+        productions = self.__grammar.ProductionsLeftToRight
+        self.__productionsRhs = list()
+
+        for key in productions.keys():
+
+            for p in productions[key]:
+                if not p[0] == "eps":
+                    self.__productionsRhs.append(p)
+                else:
+                    self.__productionsRhs.extend(["eps", key])
+
+        print(self.__productionsRhs)
+
+        for key in productions.keys():
+            for p in productions[key]:
+                first = p[0]
+
+                if first in self.__grammar.Alphabet:
+                    if self.__parseTable.get((key, first))[0] == "err":
+                        self._parseTable.update((key, first), (" " + p, self._productionsRhs.index(p) + 1))
+                    else:
+                        try:
+                            raise Exception("CONFLICT: Pair "+key+","+first)
+                        except:
+                            traceback.print_exception(*sys.exc_info())
+                elif first in self.__grammar.NonTerminals:
+                    if len(p) == 1:
+                        for symbol in self.__firstTable.get(first):
+                            if self.__parseTable.get((key, symbol))[0] == "err":
+                                self._parseTable.update((key, symbol), (" " + p, self._productionsRhs.index(p) + 1))
+                            else:
+                                try:
+                                    raise Exception("CONFLICT: Pair " + key + "," + symbol)
+                                except:
+                                    traceback.print_exception(*sys.exc_info())
+                    else:
+                        i = 1
+                        nextSymbol = p[1]
+                        firstSetForProd = self.__firstTable.get(first)
+
+                        while i < len(p) and nextSymbol in self.grammar.NonTerminals:
+                            firstForNext = self.__firstTable.get(nextSymbol)
+                            if "eps" in firstSetForProd:
+                                firstSetForProd.remove("eps")
+                                firstSetForProd.extend(firstForNext)
+
+                            i+=1
+                            if i < len(p):
+                                nextSymbol = p[i]
+
+                        for symbol in firstSetForProd:
+                            if symbol == "eps":
+                                symbol = "$"
+                            if self.__parseTable.get((key, symbol))[0] == "err":
+                                self._parseTable.update((key, symbol), (' '.join(p), self._productionsRhs.index(p) + 1))
+                            else:
+                                try:
+                                    raise Exception("CONFILCT: Pair " + key + "," + symbol)
+                                except:
+                                    traceback.print_exception(*sys.exc_info())
+                else:
+                    follow = self.__followTable.get(key)
+                    for symbol in follow:
+                        if symbol == "eps":
+                            if self.__parseTable.get((key, "$"))[0] == "err":
+                                prod = ["eps", key]
+                                self._parseTable.update((key, "$"), ("eps", self._productionsRhs.index(prod) + 1))
+                            else:
+                                try:
+                                    raise Exception("CONFILCT: Pair " + key + "," + symbol)
+                                except:
+                                    traceback.print_exception(*sys.exc_info())
+                        elif self.__parseTable.get((key, symbol))[0] == "err":
+                            prod = ["eps", key]
+                            self._parseTable.put((key, symbol), ("eps", self._productionsRhs.index(prod) + 1))
+                        else:
+                            try:
+                                raise Exception("CONFILCT: Pair " + key + "," + symbol)
+                            except:
+                                traceback.print_exception(*sys.exc_info())
 
     def initializeTable(self, table, isFollow=False):
         for nonterminal in self.__grammar.NonTerminals:
@@ -61,7 +172,8 @@ class LL_Parser(object):
                                             if firstToken != self.EPSILON:
                                                 self.__followTable[nonterminal].add(firstToken)
                                             else:
-                                                self.__followTable[nonterminal].update(previousFollowTable[production.leftHandSide])
+                                                self.__followTable[nonterminal].update(
+                                                    previousFollowTable[production.leftHandSide])
             if previousFollowTable == self.__followTable:
                 break
             previousFollowTable = copy.deepcopy(self.__followTable)
@@ -74,11 +186,64 @@ class LL_Parser(object):
         print("FOLLOW:")
         print(self.__followTable)
 
-#grammar = Grammar("g1.txt")
-grammar = Grammar("g2.txt")
+    def printParseTable(self):
+        print("PARSE TABLE:")
+        print(self.__parseTable)
 
-ll_parser = LL_Parser(grammar)
-ll_parser.first()
-ll_parser.printFirst()
-ll_parser.follow()
-ll_parser.printFollow()
+    def getProductionByOrderNr(self, nr):
+        production = self.__productionsRhs[nr - 1]
+        if "eps" in production:
+            return ["eps"]
+        return production
+
+    def readSequence(self, fileName):
+        sequence = []
+        fileContent = open(fileName, "r")
+        lines = fileContent.readlines()
+        for line in lines:
+            symbols = line.split(" ")
+            sequence.extend(symbols)
+        return sequence
+
+    def parseSequence(self, sequence):
+        alpha = []
+        beta = []
+        result = []
+        alpha.append("$")
+        i = len(sequence) - 1
+        while i >= 0:
+            alpha.append(sequence[i])
+            i -= 1
+
+        beta.append("$")
+        beta.append(self.grammar.S)
+
+        while not (alpha[len(alpha) - 1] == "$" and beta[len(alpha) - 1] == "$"):
+            alphaPeek = alpha.pop()
+            betaPeek = beta.pop()
+            value = self.__parseTable[[alphaPeek, betaPeek]]
+            if value[0] != "err":
+                if value[0] == "pop":
+                    alpha.pop()
+                    beta.pop()
+                else:
+                    beta.pop()
+                    if value[0] != "eps":
+                        val = value[0].split(" ")
+                        i2 = len(val) - 1
+                        while i2 >= 0:
+                            beta.append(val[i2])
+                            i2 = i2 - 1
+                    result.append(value[1])
+
+            else:
+                print("Syntax error for key [" + alphaPeek + ", " + betaPeek + "]")
+                print("Current alpha and beta for sequence parsing: ")
+                print(alpha)
+                print(beta)
+                return result
+
+        return result
+
+
+
